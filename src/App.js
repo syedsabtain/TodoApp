7b8c136999f22f0e3efde600989368f1dfa145f8
syed <sabtain.info@gmail.com>
2020-06-23 09:07:20 -0700

    TODO App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
 import './App.css';
+import './bootstrap/bootstrap.min.css'
+import TodoApp from './Todoapp';
+import {Globalstore} from './Datacontext'
+import Todoreducer from './Todoreducer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const initialdata = [
+        {
+            todo:'Try it yourself',
+            id:0,
+            checked:true,
+
+        },
+    ]
+    let [state,dispatch] =useReducer(Todoreducer,initialdata)
+
+     function Addtodo(obj)
+     {
+         dispatch({
+             type:"ADD_TODO",
+             payload:{
+                 todo:obj.todo,
+                 id:obj.id,
+                 checked:obj.checked
+             }
+         })
+     }
+
+     function Deletetodo(obj)
+     {
+         dispatch({
+             type:"DELETE_TODO",
+             payload:obj
+         })
+     }
+     function Handlecheck(id)
+     {
+         dispatch({
+             type:"CHECKED",
+             payload:id
+         })
+     }
+    return (
+        <Globalstore.Provider value={{store:state,addtodo:Addtodo,deletetodo:Deletetodo,handlecheck:Handlecheck}}>
+            <div className="container mt-4 ">
+            <div className=' specfic shadow-lg'>
+            <TodoApp></TodoApp>
+            </div>
+                
+            </div>
+        </Globalstore.Provider>
+
+    )
 }
 
 export default App;
